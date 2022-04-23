0304b810ad69678e14ff5e9af9ae436d6e21a110
muutualswap <75495839+muutualswap@users.noreply.github.com>
2022-04-23 13:23:14 -0400

    Update data.ts

--- src/views/Home/components/SalesSection/data.ts
+++ src/views/Home/components/SalesSection/data.ts
@@ -11,68 +11,61 @@ export const swapSectionData = (t: TranslateFunction): SalesSectionProps => ({
     external: false,
   },
   secondaryButton: {
-    to: 'https://docs.pancakeswap.finance/',
+    to: 'https://docs.bigswap.exchange/',
     text: t('Learn'),
     external: true,
   },
   images: {
     path: '/images/home/trade/',
     attributes: [
-      { src: 'BNB', alt: t('BNB token') },
-      { src: 'BTC', alt: t('BTC token') },
-      { src: 'CAKE', alt: t('CAKE token') },
+      { src: '', alt: '' },
+      { src: '', alt: '' }, 
     ],
   },
 })
 
-export const earnSectionData = (t: TranslateFunction): SalesSectionProps => ({
-  headingText: t('Earn passive income with crypto.'),
-  bodyText: t('PancakeSwap makes it easy to make your crypto work for you.'),
+export const earnSectionData: SalesSectionProps = {
+  headingText: 'Earn passive income with crypto.',
+  bodyText: 'BigSwap makes your crypto work for you.',
   reverse: true,
   primaryButton: {
     to: '/farms',
-    text: t('Explore'),
+    text: 'Explore',
     external: false,
   },
   secondaryButton: {
-    to: 'https://docs.pancakeswap.finance/products/yield-farming',
-    text: t('Learn'),
+    to: 'https://docs.bigswap.exchange/products/yield-farming',
+    text: 'Learn',
     external: true,
   },
   images: {
     path: '/images/home/earn/',
     attributes: [
-      { src: 'pie', alt: t('Pie chart') },
-      { src: 'stonks', alt: t('Stocks chart') },
-      { src: 'folder', alt: t('Folder with cake token') },
+      { src: '', alt: '' },
     ],
   },
-})
+}
 
-export const cakeSectionData = (t: TranslateFunction): SalesSectionProps => ({
-  headingText: t('CAKE makes our world go round.'),
-  bodyText: t(
-    'CAKE token is at the heart of the PancakeSwap ecosystem. Buy it, win it, farm it, spend it, stake it... heck, you can even vote with it!',
-  ),
+export const cakeSectionData: SalesSectionProps = {
+  headingText: 'BGSP makes our world go round.',
+  bodyText:
+    'BGSP token is the heart of the BigSwap ecosystem. Buy it, Farm it, Spend it, Stake it, you can even Vote with it!',
   reverse: false,
   primaryButton: {
-    to: '/swap?outputCurrency=0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
-    text: t('Buy CAKE'),
+    to: '/swap?outputCurrency=0xe9f861b22cd5240494ecc14afea02659ec59864f',
+    text: 'Buy BGSP',
     external: false,
   },
   secondaryButton: {
-    to: 'https://docs.pancakeswap.finance/tokenomics/cake',
-    text: t('Learn'),
+    to: 'https://docs.bigswap.exchange/tokenomics/BGSP',
+    text: 'Learn',
     external: true,
   },
 
   images: {
     path: '/images/home/cake/',
     attributes: [
-      { src: 'bottom-right', alt: t('Small 3d pancake') },
-      { src: 'top-right', alt: t('Small 3d pancake') },
-      { src: 'coin', alt: t('CAKE token') },
-      { src: 'top-left', alt: t('Small 3d pancake') },
+      { src: '', alt: '' }, 
     ],
-  },
-})
+   },
+})  
