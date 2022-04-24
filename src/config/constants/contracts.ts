bdd174e17c48c2f19a7d28b208b0816f0d599927
BigSwap Finance <86725876+BigSwap-Finance@users.noreply.github.com>
2022-04-23 20:23:43 -0400

    Update contracts.ts

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -5,7 +5,7 @@ export default {
   },
   masterChefV1: {
     97: '0x1d32c2945C8FDCBc7156c553B7cEa4325a17f4f9',
-    56: '0x73feaa1eE314F8c655E354234017bE2193C9E24E',
+    56: '0x65FE214382fD9e0d6cF054EeD65C150C88ed6834',
   },
   sousChef: {
     97: '0xd3af5fe61dbaf8f73149bfcfa9fb653ff096029a',
